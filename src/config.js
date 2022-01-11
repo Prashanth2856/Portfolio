25deb9d83c872131f771871b43e9ac4436b3848f
Prashanth2856 <prashanthnani2856@gmail.com>
2022-01-11 22:43:52 +0530

    My Portfolio

---
+++ src/config.js
@@ -0,0 +1,165 @@
+module.exports = {
+  email: 'prashanthdyavanapally67@gmail.com',
+
+  logo:
+    'M 32.55 49.63 L 29.82 42.56 L 12.39 42.56 L 9.8 49.63 L 0 49.63 L 20.65 0 L 21.35 0 L 42.07 49.63 L 32.55 49.63 Z M 14.91 35.77 L 27.23 35.77 L 22.12 22.47 L 20.93 18.55 L 19.81 22.47 L 14.91 35.77 Z',
+
+  logo_alphabet_g_position: 'translate(29.000000, 19.000000)',
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/Prashanth2856',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/itzme_prashanth_7872/',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/PrashanthDyava6',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/prashanth-dyavanapally-3a681920b/',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Skills',
+      url: '/#skills',
+    },
+    {
+      name: 'Projects',
+      url: '/#projects',
+    },
+    {
+      name: 'Blogs',
+      url: '/#blogs',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+  tech: [
+    {
+      name: 'javascript',
+      img: 'https://cdn.iconscout.com/icon/free/png-512/javascript-2038874-1720087.png',
+    },
+    {
+      name: 'React',
+      img: 'https://cdn.iconscout.com/icon/free/png-512/react-4-1175110.png',
+    },
+    {
+      name: 'Redux',
+      img: 'https://cdn.iconscout.com/icon/free/png-512/redux-3629018-3030243.png',
+    },
+    {
+      name: 'nodejs',
+      img: 'https://cdn.iconscout.com/icon/free/png-512/node-js-3-1174937.png',
+    },
+    {
+      name: 'mongoDB',
+      img: 'https://cdn.iconscout.com/icon/free/png-512/mongodb-5-1175140.png',
+    },
+    {
+      name: 'HTML',
+      img: 'https://cdn.iconscout.com/icon/free/png-64/html-2752158-2284975.png',
+    },
+    {
+      name: 'CSS',
+      img: 'https://cdn.iconscout.com/icon/free/png-64/css-131-722685.png',
+    },
+    {
+      name: 'expressJs',
+      img: 'https://pngimage.net/wp-content/uploads/2018/05/express-js-png-7.png',
+    },
+  ],
+  blogs: [
+    {
+      frontmatter: {
+        external: 'https://prashanthdyavanapally.hashnode.dev/seekho-the-reading-app',
+        github: 'https://github.com/Prashanth2856/seekho-project',
+        title: 'Seekho-the Reading App',
+      },
+      html:
+        '<p>Hello readers, hope you all are doing pretty fine. So this blog is based on our unit-4 construct week project and believe me on that this time the challenge was pretty tough and interesting as the project is a combination of Web-Designer and Web-Developers. We haven’t cloned anything like we use to do in previous units, this is our very first self made product application.</p>',
+    },
+    {
+      frontmatter: {
+        external: 'https://prashanthdyavanapally.hashnode.dev/clone-of-purpllecom',
+        github: 'https://github.com/bhawaniCodes/Purplle-final-with-Database',
+        title: 'Clone of Purplle.com',
+      },
+      html:
+        '<p> Hello readers, I hope you are doing great. I would like you to introduce myself first. I am Prashanth Dyavanapally , currently I am learning Full-Stack Web Development at Masai School. In this blog, I would like to share the project which has assigned to us in the construct week of unit-3 in masai school that how I and my team build in 5 days. </p>',
+    },
+    {
+      frontmatter: {
+        external:
+          'https://prashanthdyavanapally.hashnode.dev/clone-theskinstorein-using-frontend-and-backend',
+        github: 'https://github.com/Prashanth2856/theskinstore_frontend_backend',
+        title: 'Clone Theskinstore.in using frontend & backend',
+      },
+      html:
+        '<p>In this article, you will get insights into how I was able to clone the Theskinstore. website in just 3 days.It is highly difficult to clone & get the exact model of the original website in 3 days but yes, I tried my best and the results I have achieved were tremendous.</p>',
+    },
+  ],
+
+  projects: [
+    {
+      title: 'Seekho - the reading app',
+      tech: ['React', 'Javascript', 'Express', 'Node', 'MongoDb'],
+      github: 'https://github.com/vcow10kar/seekho-project',
+      image: 'https://i.ibb.co/xzBt5v4/seekho.jpg',
+      link: 'https://seekho-frontend-vcow10kar.vercel.app/',
+      html:
+        '<p>Seekho is an e-learning platform which allows user to read and download there desired books based on there choice. It also has features of user sign-in and sign-up and allows user to resume reading from where they left.</p>',
+    },
+    {
+      title: 'Purplle.com - clone',
+      tech: ['HTML', 'CSS', 'Javascript', 'Express', 'Node', 'MongoDb'],
+      github: 'https://github.com/bhawaniCodes/Purplle-final-with-Database',
+      link: 'https://frozen-castle-82316.herokuapp.com/',
+      image: 'https://i.ibb.co/wCvCy67/purplle.png',
+      html:
+        '<p>Purplle is an online portal with a very large collection of cosmetics, skincare, hair care & fragrances. It showcases some of the best products from the leading brands in the world at the best price.</p>',
+    },
+    {
+      title: 'Theskinstore.in - clone',
+      tech: ['HTML', 'CSS', 'Javascript', 'Express', 'Node', 'MongoDb'],
+      github: 'https://github.com/Prashanth2856/theskinstore_frontend_backend',
+      link: 'https://shielded-thicket-15248.herokuapp.com/',
+      image: 'https://i.ibb.co/vHpB2cv/theskinstore.png',
+      html:
+        '<p>Theskinstore.in is an online portal with a very large collection of cosmetics, skincare, hair care, Baby care, Home Supplies & fragrances.Products are also categorised by their key purpose or use, to help you find what you need more easily.</p>',
+    },
+  ],
+
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+
+  srConfig: (delay = 200, viewFactor = 0.25) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+};
